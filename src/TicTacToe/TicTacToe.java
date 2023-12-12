b517666c0ca369736332a12ed6f1dcc75866b03e
Максим <Maxim>
2023-12-12 16:41:51 +0200

    TicTacToe 4-st stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -16,66 +16,50 @@ public class TicTacToe {
         }
 
         // Виведення ігрового поля
+        printGrid(input);
+
+        // Запит на введення координат від користувача
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            try {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+
+                if (x < 1 || x > 3 || y < 1 || y > 3) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    scanner.nextLine(); // Очистка буфера введення
+                    continue;
+                }
+
+                int index = (3 - y) * 3 + (x - 1);
+                if (input.charAt(index) != '_') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                    continue;
+                }
+
+                input = input.substring(0, index) + 'X' + input.substring(index + 1);
+                break;
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+                scanner.nextLine(); // Очистка буфера введення
+            }
+        }
+
+        // Оновлення та виведення ігрового поля після ходу користувача
+        printGrid(input);
+    }
+
+    // Функція виведення ігрового поля
+    public static void printGrid(String cells) {
         System.out.println("---------");
         for (int i = 0; i < 9; i += 3) {
             System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                char symbol = input.charAt(i + j);
+                char symbol = cells.charAt(i + j);
                 System.out.print(symbol + " ");
             }
             System.out.println("|");
         }
         System.out.println("---------");
-
-        // Аналіз стану гри
-        boolean xWins = checkWinner(input, 'X');
-        boolean oWins = checkWinner(input, 'O');
-        boolean impossible = isImpossible(input);
-
-        if (impossible) {
-            System.out.println("Impossible");
-        } else if (xWins) {
-            System.out.println("X wins");
-        } else if (oWins) {
-            System.out.println("O wins");
-        } else if (input.contains("_")) {
-            System.out.println("Game not finished");
-        } else {
-            System.out.println("Draw");
-        }
-    }
-
-    // Перевірка переможця
-    public static boolean checkWinner(String cells, char symbol) {
-        // Перевірка рядків, стовпців та діагоналей
-        return (cells.charAt(0) == symbol && cells.charAt(1) == symbol && cells.charAt(2) == symbol) ||
-                (cells.charAt(3) == symbol && cells.charAt(4) == symbol && cells.charAt(5) == symbol) ||
-                (cells.charAt(6) == symbol && cells.charAt(7) == symbol && cells.charAt(8) == symbol) ||
-                (cells.charAt(0) == symbol && cells.charAt(3) == symbol && cells.charAt(6) == symbol) ||
-                (cells.charAt(1) == symbol && cells.charAt(4) == symbol && cells.charAt(7) == symbol) ||
-                (cells.charAt(2) == symbol && cells.charAt(5) == symbol && cells.charAt(8) == symbol) ||
-                (cells.charAt(0) == symbol && cells.charAt(4) == symbol && cells.charAt(8) == symbol) ||
-                (cells.charAt(2) == symbol && cells.charAt(4) == symbol && cells.charAt(6) == symbol);
-    }
-
-    // Перевірка неможливості стану гри
-    public static boolean isImpossible(String cells) {
-        int countX = 0;
-        int countO = 0;
-
-        for (char cell : cells.toCharArray()) {
-            if (cell == 'X') {
-                countX++;
-            } else if (cell == 'O') {
-                countO++;
-            }
-        }
-
-        int diff = Math.abs(countX - countO);
-        boolean xWins = checkWinner(cells, 'X');
-        boolean oWins = checkWinner(cells, 'O');
-
-        return (xWins && oWins) || diff >= 2;
     }
 }
-
