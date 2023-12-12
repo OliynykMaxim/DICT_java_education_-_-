91f6f7eb865e4f827d1a3921f565a738a2f1d6f9
Максим <Maxim>
2023-12-12 16:37:25 +0200

    TicTacToe 2-st stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,20 +1,31 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        // Ігрове поле
-        char[][] gameBoard = {
-                {'X', 'O', 'X'},
-                {'O', 'X', 'O'},
-                {'X', 'X', 'O'}
-        };
+        Scanner scanner = new Scanner(System.in);
 
-        // Вивід ігрового поля
-        for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j < gameBoard[i].length; j++) {
-                System.out.print(gameBoard[i][j] + " ");
-            }
-            System.out.println();
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        // Перевірка на правильність введення
+        if (input.length() != 9) {
+            System.out.println("Incorrect input! Please enter 9 characters.");
+            return;
         }
+
+        // Виведення ігрового поля
+        System.out.println("---------");
+        for (int i = 0; i < 9; i += 3) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                char symbol = input.charAt(i + j);
+                System.out.print(symbol + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
+
